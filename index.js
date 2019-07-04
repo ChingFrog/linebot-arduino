c7c44e433993225d6a0521311a830c7835cfa94b
Ken <a1c2a1c2a1c22@gmail.com>
2019-07-04 11:36:06 +0800

    ‘’

--- index.js
+++ index.js
@@ -25,11 +25,18 @@ app.get('/',function(req,res){
 app.post('/linewebhook', linebotParser);
 
 bot.on('message', function (event) {
+	event.reply(outputData).then(function (data) {
+		console.log('Success', data);
+	}).catch(function (error) {
+		console.log('Error', error);
+	});
+	/*
 	event.reply(event.message.text+event.message.text).then(function (data) {
 		console.log('Success', data);
 	}).catch(function (error) {
 		console.log('Error', error);
 	});
+	*/
 });
 
 app.listen(process.env.PORT || 80, function () {
